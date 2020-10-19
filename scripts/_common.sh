64424b00c8e12f8798335298a2ae9c5aa32c9bd9
ericgaspar <junk.eg@free.fr>
2020-10-19 14:26:54 +0200

    Upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="python-pip python-virtualenv python-dev libxml2-dev \
+	libxslt-dev libjpeg-dev libffi-dev zlib1g-dev \
+	`# add dbus glib gobject-introspection and cairo2` \
+	dbus-x11 libglib2.0-dev libgirepository1.0-dev libcairo2-dev"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
